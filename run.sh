0396633919624f8daedf07123948c9ac6d708503
root <root@dev.hzero.org>
2020-03-04 17:23:02 +0800

    xxx

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ mv app.jar $JAR
 echo ">>> kill -9 $(lsof -t -sTCP:LISTEN -i:$MPORT)"
 kill -9 $(lsof -t -sTCP:LISTEN -i:$MPORT)
 
-JAVA_OPTS="-Xms512m -Xmx1536m -Dspring.profiles.active=dev"
+JAVA_OPTS="-Xms200m -Xmx200m -Dspring.profiles.active=dev"
 echo ">>> nohup java -jar $JAVA_OPTS $JAR >app.log &"
 BUILD_ID=dontKillMe nohup java -jar $JAVA_OPTS $JAR >app.log &
 
